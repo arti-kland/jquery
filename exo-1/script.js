82556312baf7d32b521fef796072c3b920b45abb
jpmauviel <jean-philippe.mauviel@le-campus-numerique.fr>
2019-04-08 10:04:33 +0200

    exercice 1

---
+++ exo-1/script.js
@@ -0,0 +1,22 @@
+console.log('exercice 1');
+
+/*JavaScript est un langage de programmation alors que jQuery est une bibliothèque JavaScript open-source inventée.
+Insérer du JavaScript pur dans nos projets web peut changer le comportement de nos pages.
+Ce langage vient compléter le HTML et le CSS de notre site, qui déterminent le contenu et l'apparence de votre page, en y ajoutant une couche d'interactivité.
+jQuery permet d'économiser une tonne d'énergie en effectuant des choses qui prendraient une éternité en JavaScript ordinaire. 
+jQuery nous permet d'exploiter la puissance de JavaScript pour accomplir une myriade de choses géniales sur notre page web.
+Avec jQuery, on peu par exemple :
+Ajouter, supprimer ou modifier des éléments HTML au sein de votre page.
+Changer les styles des éléments de la page en modifiant le CSS qui leur est associé.
+Animer des éléments de votre page.
+Envoyer et recevoir des données depuis un serveur grâce à AJAX (asynchronous JavaScript and XML, c'est-à-dire JavaScript et XML asynchrones) pour ne plus avoir besoin de recharger vos pages après validation d'un formulaire.
+Et bien plus encore !
+jQuery nous permet de profiter d'une plus grande compatibilité avec les différents navigateurs.
+Le JavaScript pur est réputé pour avoir des spécificités dans chaque navigateur comme Internet Explorer, Chrome, Safari, etc, qui vont potentiellement exécuter votre code JavaScript différemment les uns des autres.
+jQuery vous évite cette prise de tête, car il est conçu dans un esprit de compatibilité maximale.
+Cette compatibilité est rendue possible par l'utilisation que fait jQuery des sélecteurs CSS.
+Sélectionner des éléments devient plus rapide et plus direct qu'en JavaScript classique, et puisque la plupart des développeurs web comprennent déjà les sélecteurs CSS, ils n'ont aucun mal à se servir de jQuery.*/
+
+$(document).ready(function(){
+    console.log('Je peux maintenant écrire du code JQuery');
+});
