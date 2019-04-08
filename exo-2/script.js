7310d633c7615851d5798679b4505e7e0c2f17f1
jpmauviel <jean-philippe.mauviel@le-campus-numerique.fr>
2019-04-08 12:52:38 +0200

    exrecice 2 tout pourris

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,19 @@
 console.log('exercice 2');
 
+
+$(document).ready(function(show){
+    console.log($("#title"));
+    console.log($("li"));
+    console.log($("footer "));
+    console.log($(".input-group"));
+    console.log($(".glyphicon"));
+    console.log($(".label-success"));
+    console.log($("p.article"));
+    console.log($(".glyphicon-time"));
+    console.log($(".sidenav li:first"));
+    console.log($("p:odd"));
+});
+
+
+
+
